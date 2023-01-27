41c7463cf5c260732a74aeeb5a5f4c7ab665db54
Zwea Htet <zhtet5@berkeley.edu>
2023-01-27 12:11:26 -0800

    completed and passed tests for task 2

--- proj1.sql
+++ proj1.sql
@@ -19,49 +19,72 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE namefirst LIKE '% %'
+  ORDER BY namefirst ASC, namelast ASC
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear 
+  ORDER BY birthyear ASC
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, avgheight, count
+  FROM q1iii
+  WHERE avgheight > 70
+  ORDER BY birthyear ASC
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.namefirst, p.namelast, p.playerID, h.yearid
+  FROM people p, halloffame h
+  WHERE (p.playerID = h.playerID) and (h.inducted = 'Y')
+  ORDER BY h.yearid DESC,  p.playerID
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT q.namefirst, q.namelast, q.playerID, c.schoolID, q.yearid
+  FROM q2i q, collegeplaying c, schools s
+  WHERE s.schoolState = 'CA' 
+    AND c.playerid = q.playerID
+    AND c.schoolID = s.schoolID
+  ORDER BY q.yearid DESC, c.schoolID, q.playerID
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT q2i.playerID, q2i.namefirst, q2i.namelast, c.schoolID 
+  FROM q2i LEFT JOIN collegeplaying c 
+    ON q2i.playerID = c.playerid
+  ORDER BY q2i.playerID DESC, c.schoolID 
 ;
 
 -- Question 3i
